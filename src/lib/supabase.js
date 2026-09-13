0e6491f89898265dbb9cc1e516248e4eb96b0678
publicaxyz <marvinlucas486@gmail.com>
2026-09-13 01:41:06 -0400

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,1 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
 
+const supabaseUrl = 'https://rfjtpgzsnmgpmhtvnrrm.supabase.co'
+const supabaseAnonKey = 'sb_publishable_NpaIhQ9S2yg8IsRVFGsA_g_eWgf3i4u' 
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
