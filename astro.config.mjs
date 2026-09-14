566e4d861a9195c262aeeeafd626d0c7d1c620f1
publicaxyz <marvinlucas486@gmail.com>
2026-09-14 01:59:37 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
+  output: 'server', // <-- ESTO ES CLAVE PARA QUE FUNCIONEN LAS APIS
+  adapter: vercel(), // <-- ESTO CONECTA CON VERCEL
   base: '/',
   vite: {
     server: {
