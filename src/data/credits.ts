8613ecbb04c4fb95b9bdf20269fdbcfb45e8d748
publicaxyz <marvinlucas486@gmail.com>
2026-09-17 00:32:43 -0400

    Change CREDIT_URL to new site link
    Updated the CREDIT_URL to point to the new site.

--- src/data/credits.ts
+++ src/data/credits.ts
@@ -1,6 +1,6 @@
 export const CREDIT_NAME = "Ade";
 export const CREDIT_TEXT = "Hecho con ❤️ por";
-export const CREDIT_URL = "https://www.instagram.com/bazarvilladigital"; // <--- Agrega esta línea
+export const CREDIT_URL = "https://bazarvilladigital.vercel.app"; // <--- Agrega esta línea
 export const SOCIAL_LINKS = [
   { name: "Instagram", url: "https://www.instagram.com/bazarvilladigital" },
   { name: "Facebook", url: "https://www.facebook.com/bazarvilladigital" },
